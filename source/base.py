b874dedb86d4df1df6be8d43781ef97c49bdb3ca
Rinslep <4941570+Rinslep@users.noreply.github.com>
2020-03-24 12:40:23 +0000

    Add files via upload

--- source/base.py
+++ source/base.py
@@ -68,7 +68,7 @@ class Fluid_Handler(object):
     Fluids wont be put in for a while, but this placeholder means I can structure the program better for the future.
     Will need to have a fluid dict and add it to update_files
     """
-    handles_fluids = True
+    pass
 
 
 class Drill(Machine):
@@ -98,69 +98,66 @@ class Furnace(Assembly):
     pass
 
 
-class Item(object):
-    items_dict = {}
-    is_raw = False
-
-    def __init__(self, name: str, machine_type_crafted_in=Assembly):
-        self.name = name
-        self.crafted_in = machine_type_crafted_in
-
-        if self.name not in Item.items_dict.keys():
-            Item.items_dict[self.name] = self
-
-        # if isinstance(self.crafted_in, Drill): self.is_raw = True
-
-    def __repr__(self):
-        return "{}('{}', {})".format(type(self).__name__, self.name, self.crafted_in)
-
-
-class Raw_Resource(Item):
-    """
-    Any item that doesn't have an assembler Recipe should be here
-    Examples would be copper/iron ore, oil, water etc.
-    """
-    is_raw = True
-
-
 class Recipe(object):
     recipes_dict = {}
+    multi_crafting_list = []
 
-    def __init__(self, time_in_seconds: float, output: tuple, inputs: tuple):
+    def __init__(self, time_in_seconds: float, output: tuple, inputs: tuple, name=None):
         """
         output and inputs expects a tuple of format (item:Item, quantity:int)
         """
         self.time = time_in_seconds
         self.output = output
         self.inputs = inputs
+        if name is not None:
+            self.name = name
+        else:
+            self.name = self.output[0]
+        # for item in self.get_output_item():
+        #     item.add_possible_recipe(self)
+        if self.num_outputs == 1:
+            self.output[0].add_possible_recipe(self)
+        else:
+            for item, quantity in self.output:
+                item.add_possible_recipe(self)
 
-        if self.item not in Recipe.recipes_dict.keys():
-            Recipe.recipes_dict[self.item] = self  # classes should add themselves to dict with no duplicates
+
+        # if self.item in Recipe.recipes_dict.keys():
+        #     if not self.inputs == Recipe.recipes_dict[self.item]:  # if not same recipe
+        #         self.multi_crafting_list.append(self.item)  # add to list of items with multiple recipes
+        #     else:
+        #         self.item.add_possible_recipe(self)
+        # else:
+        #     Recipe.recipes_dict[self.item] = self  # classes should add themselves to recipes_dict with no duplicates
+        #     self.item.add_possible_recipe(self)
 
     def __repr__(self):
         return "{}({},{},{})".format(type(self).__name__, self.time, self.output, self.inputs)
-
-    @property
-    def item(self):
-        return self.output[0]
 
     @property
     def output_quantity(self):
         return self.output[1]
 
     @property
-    def num_inputs(self):  # can be replaced with len? does it make sense to do so?
-        if len(self.inputs) == 2 and not isinstance(self.inputs[0], tuple):
+    def num_inputs(self):
+        if not isinstance(self.inputs[0], tuple):
             return 1
         else:
             return len(self.inputs)
 
-    @staticmethod
-    def get_recipe_from_string(s: str):
-        return Recipe.recipes_dict[Item.items_dict[s]]
+    @property
+    def num_outputs(self):
+        if not isinstance(self.output[0], tuple):
+            return 1
+        else:
+            return len(self.output)
 
     @staticmethod
-    def get_recipe(item: [Item, str]):
+    def get_recipe_from_string(s: str):
+        return Item.get_item_from_string(s).recipes[0]
+
+    @staticmethod
+    def get_recipe(item):
         """
         :param item: Item or String
         :return: Recipe of item
@@ -168,9 +165,15 @@ class Recipe(object):
         if isinstance(item, str):
             return Recipe.get_recipe_from_string(item)
         else:
-            return Recipe.recipes_dict[item]
+            return item.get_recipe()
 
-    def get_ratio(self, input_item: Item):
+    @staticmethod
+    def has_multi(input_item):
+        if input_item in Recipe.multi_crafting_list:
+            return True
+        else: return False
+
+    def get_ratio(self, input_item):
         if self.num_inputs == 1:
             if self.inputs[0] is input_item:
                 return self.inputs[1] / self.output_quantity
@@ -178,6 +181,77 @@ class Recipe(object):
             for item, quantity in self.inputs:
                 if item is input_item:
                     return quantity / self.output_quantity
+
+    def get_input_item(self):
+        if self.num_inputs == 1:
+            yield self.inputs[0]
+            # yield StopIteration
+        else:
+            for input, quantity in self.inputs:
+                yield input
+            # yield StopIteration
+
+    def get_output_item(self):
+        if self.num_outputs == 1:
+            return self.output[0]
+            # yield StopIteration
+        else:
+            for output, quantity in self.output:
+                yield output
+            # yield StopIteration
+
+
+class Item(object):
+    items_dict = {}
+    is_raw = False
+    is_fluid = False
+
+
+    def __init__(self, name: str, machine_type_crafted_in=Assembly):
+        self.name = name
+        self.crafted_in = machine_type_crafted_in
+        self.recipes = []
+        if self.name not in Item.items_dict.keys():
+            Item.items_dict[self.name] = self
+
+        if isinstance(self, Raw_Resource) or issubclass(type(self), Raw_Resource):
+            self.is_raw = True
+
+        if issubclass(type(self), Fluid_Handler):
+            self.is_fluid = True
+
+    def __repr__(self):
+        return "{}('{}', {})".format(type(self).__name__, self.name, self.crafted_in)
+
+    def add_possible_recipe(self, recipe):
+        self.recipes.append(recipe)
+        print(f"{self}: {recipe.name}")
+
+    @property
+    def has_multi(self):
+        return len(self.recipes) > 1
+
+    @staticmethod
+    def get_item_from_string(s: str):
+        return Item.items_dict[s]
+
+    def get_recipe(self):
+        return self.recipes[0]
+
+
+class Raw_Resource(Item):
+    """
+    Any item that doesn't have an assembler Recipe should be here # maybe not?
+    Examples would be copper/iron ore, oil, water etc.
+    """
+
+
+class Fluid_Item(Item, Fluid_Handler):
+    pass
+
+
+class Fluid_Raw_Resource(Raw_Resource, Fluid_Handler):
+    pass
 
 
 class Fluid_Recipe(Recipe, Fluid_Handler):
@@ -203,8 +277,34 @@ def update_files():
 
 
 def init_test_data(file_overwrite=False):
-    mining_drill = Drill("mining_drill", 0.5, 3)
-    stone_furnace = Furnace("stone_furnace", 1, 0)
+    m_mining_drill = Drill("mining_drill", 0.5, 3)
+    m_stone_furnace = Furnace("stone_furnace", 1, 0)
+    m_boiler = Fluid_Assembly("boiler", 1, 0)
+
+    coal = Raw_Resource("coal", Drill)
+    r_coal = Recipe(1, (coal, 1), (coal, 1))
+
+    water = Fluid_Raw_Resource("water", Pump)
+    f_r_water = Fluid_Recipe(1, (water, 1200), (water, 1200))
+
+    steam = Fluid_Item("steam", Fluid_Assembly)
+    f_r_steam = Fluid_Recipe(1, (steam, 60), (water, 60))
+
+    crude_oil = Fluid_Raw_Resource("crude_oil", Pump)
+    f_r_crude_oil = Fluid_Recipe(1, (crude_oil, 100), (crude_oil, 100))
+
+    heavy_oil = Fluid_Item("heavy_oil", Fluid_Assembly)
+    light_oil = Fluid_Item("light_oil", Fluid_Assembly)
+    petroleum_gas = Fluid_Item("petroleum_gas", Fluid_Assembly)
+
+    basic_oil = Fluid_Recipe(5, (petroleum_gas, 45), (crude_oil, 100), "basic_oil_processing")
+    advanced_oil = Fluid_Recipe(5, ((heavy_oil, 25), (light_oil, 45), (petroleum_gas, 55)),
+                                ((crude_oil, 100), (water, 50)), "advanced_oil_processing")
+    coal_liquefaction = Fluid_Recipe(5, ((heavy_oil, 90), (light_oil, 20), (petroleum_gas, 10)),
+                                     ((coal, 10), (heavy_oil, 25), (steam, 50)), "coal_liquefaction")
+
+    heavy_cracking = Fluid_Recipe(2, (light_oil, 30), ((heavy_oil, 40), (water, 30)))
+    light_cracking = Fluid_Recipe(2, (petroleum_gas, 20), ((light_oil, 30), (water, 30)))
 
     copper_ore = Raw_Resource("copper_ore", Drill)
     r_copper_ore = Recipe(1, (copper_ore, 1), (copper_ore, 1))
@@ -212,7 +312,7 @@ def init_test_data(file_overwrite=False):
     copper_plate = Item("copper_plate", Furnace)
     r_copper_plate = Recipe(3.2, (copper_plate, 1), (copper_ore, 1))
 
-    copper_cable = Item("copper_cable", Assembly)
+    copper_cable = Item("copper_cable")
     r_copper_cable = Recipe(0.5, (copper_cable, 2), (copper_plate, 1))
 
     iron_ore = Raw_Resource("iron_ore", Drill)
@@ -221,13 +321,13 @@ def init_test_data(file_overwrite=False):
     iron_plate = Item("iron_plate", Furnace)
     r_iron_plate = Recipe(3.2, (iron_plate, 1), (iron_ore, 1))
 
-    iron_gear_wheel = Item("iron_gear_wheel", Assembly)
+    iron_gear_wheel = Item("iron_gear_wheel")
     r_iron_gear_wheel = Recipe(0.5, (iron_gear_wheel, 1), (iron_plate, 2))
 
-    electronic_circuit = Item("electronic_circuit", Assembly)
+    electronic_circuit = Item("electronic_circuit")
     r_electronic_circuit = Recipe(0.5, (electronic_circuit, 1), ((copper_cable, 3), (iron_plate, 1)))
 
-    automation_science_pack = Item("automation_science_pack", Assembly)
+    automation_science_pack = Item("automation_science_pack")
     r_automation_science_pack = Recipe(5, (automation_science_pack, 1), ((copper_plate, 1), (iron_gear_wheel, 1)))
 
     transport_belt = Item("transport_belt")
@@ -238,9 +338,6 @@ def init_test_data(file_overwrite=False):
 
     logistic_science_pack = Item("logistic_science_pack")
     r_logistic_science_pack = Recipe(6, (logistic_science_pack, 1), ((inserter, 1), (transport_belt, 1)))
-
-    coal = Raw_Resource("coal", Drill)
-    r_coal = Recipe(1, (coal, 1), (coal, 1))
 
     grenade = Item("grenade")
     r_grenade = Recipe(8, (grenade, 1), ((coal, 10), (iron_plate, 5)))
@@ -268,6 +365,43 @@ def init_test_data(file_overwrite=False):
     r_military_science_pack = Recipe(10, (military_science_pack, 2), ((grenade, 1), (piercing_rounds_magazine, 1),
                                                                       (stone_wall, 2)))
 
+    pipe = Item("pipe")
+    r_pipe = Recipe(0.5, (pipe, 1), (iron_plate, 1))
+
+    engine_unit = Item("engine_unit")
+    r_engine_unit = Recipe(10, (engine_unit, 1), ((iron_gear_wheel, 1), (pipe, 2), (steel_plate, 1)))
+
+    sulfur = Item("sulfur", Fluid_Assembly)
+    f_r_sulfur = Fluid_Recipe(1, (sulfur, 2), ((petroleum_gas, 30), (water, 30)))
+
+    plastic_bar = Item("plastic_bar", Fluid_Assembly)
+    f_r_plastic_bar = Fluid_Recipe(1, (plastic_bar, 2), ((coal, 1), (petroleum_gas, 20)))
+
+    advanced_circuit = Item("advanced_circuit")
+    r_advanced_circuit = Recipe(6, (advanced_circuit, 1), ((copper_cable, 4), (electronic_circuit, 2),
+                                                           (plastic_bar, 2)))
+
+    chemical_science_pack = Item("chemical_science_pack")
+    r_chemical_science_pack = Recipe(24, (chemical_science_pack, 2), ((advanced_circuit, 3), (engine_unit, 2),
+                                                                      (sulfur, 1)))
+
+    iron_stick = Item("iron_stick")
+    r_iron_stick = Recipe(0.5, (iron_stick, 2), (iron_plate, 1))
+
+    rail = Item("rail")
+    r_rail = Recipe(0.5, (rail, 2), ((iron_stick, 1), (steel_plate, 1), (stone, 1)))
+
+    productivity_module = Item("productivity_module")
+    r_productivity_module = Recipe(15, (productivity_module, 1), ((advanced_circuit, 5), (electronic_circuit, 5)))
+
+    m_electric_furnace = Furnace("electric_furnace", 2, 2)
+    electric_furnace = Item("electric_furnace")
+    r_electric_furnace = Recipe(5, (electric_furnace, 1), ((advanced_circuit, 5), (steel_plate, 10), (stone_brick, 10)))
+
+    production_science_pack = Item("production_science_pack")
+    r_production_science_pack = Recipe(21, (production_science_pack, 3), ((electric_furnace, 1),
+                                                                          (productivity_module, 1), (rail, 30)))
+
     if file_overwrite: update_files()
 
 
@@ -286,7 +420,7 @@ def add_to_dict(dict_to_add_to, item: Item, quantity: int):
         dict_to_add_to[item] = cur_quantity + quantity
 
 
-def recipe_crawler(recipe: Recipe or str, total_dict=None, number_to_be_crafted=None, first_item=False) -> dict:
+def recipe_crawler(recipe: Recipe, total_dict=None, number_to_be_crafted=None, is_first_item=False) -> dict:
     """
     dictionary is edited in place
     """
@@ -295,36 +429,31 @@ def recipe_crawler(recipe: Recipe or str, total_dict=None, number_to_be_crafted=
     if number_to_be_crafted is None:
         number_to_be_crafted = recipe.output_quantity
 
-    output_item = recipe.output[0]
-    if first_item: add_to_dict(total_dict, output_item, number_to_be_crafted)
+    if is_first_item:
+        for output_item in recipe.get_output_item():
+            add_to_dict(total_dict, output_item, number_to_be_crafted)  # can be added if total_dict is none
 
-    if recipe.num_inputs == 1:  # only has a single item,quantity tuple
-        input_item = recipe.inputs[0]
+    for input_item in recipe.get_input_item():  # some form of generator
         modified_quantity = number_to_be_crafted * recipe.get_ratio(input_item)
-        if not input_item.is_raw:  # if 1, non-raw, input.
-            new_recipe = Recipe.get_recipe(input_item)
-            add_to_dict(total_dict, input_item, modified_quantity)
-            recipe_crawler(new_recipe, total_dict, modified_quantity)
-        else:
-            add_to_dict(total_dict, input_item, modified_quantity)
-    else:  # otherwise it will have multiple item,quantity tuples and i can loop through them
-        for input_item, input_quantity in recipe.inputs:
-            modified_quantity = number_to_be_crafted * recipe.get_ratio(input_item)
-            if not input_item.is_raw:
+        if not input_item.is_raw:  # if 1, non-raw or non-fluid, input.
+            if input_item.has_multi:  # handles oil production or anything that has multiple recipes
+                # do multi_crafting things
+                print(f"multi: {input_item}")
+            else:
                 new_recipe = Recipe.get_recipe(input_item)
                 add_to_dict(total_dict, input_item, modified_quantity)
                 recipe_crawler(new_recipe, total_dict, modified_quantity)
-            else:  # should only reach this on things like grenades/concrete
-                # print("{}, WE HIT A GRENADE!\n\n".format(recipe.output[0]))
-                add_to_dict(total_dict, input_item, modified_quantity)
+        else:  # should handle raw resources like coal and ores
+            add_to_dict(total_dict, input_item, modified_quantity)
+
     return total_dict
 
 
 read_or_write_data()
 
-r = Recipe.get_recipe("military_science_pack")
+r = Recipe.get_recipe("plastic_bar")
 
-t_d = recipe_crawler(r, None, 1.0, True)
+t_d = recipe_crawler(r, None, 3.0, True)
 
 for item in t_d.items():
     print(item)
